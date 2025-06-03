9cc95ab7cc6cf0b859a6e113c7cb2e503514fc10
CuberHuber <lupashcko.rom@yandex.ru>
2025-06-03 13:51:08 +0300

    feature (project): Dev environment
    - create dev compose file (use db container from ghcr)
    - use ghcr pre-build s3p-database image

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ load_dotenv()
 from src.app.main import app  # Импортируем приложение из указанного пути
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
